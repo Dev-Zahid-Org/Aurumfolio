68c81bbfb0d7fb13bec3e234e82bf588743fcfa6
FLYMIZ <flymiz84@gmail.com>
2025-07-11 19:27:28 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,6 +2,7 @@ import { Github, Linkedin, Twitter } from "lucide-react";
 import { Logo } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { CurrentYear } from "./current-year";
 
 export function Footer() {
   return (
@@ -10,7 +11,7 @@ export function Footer() {
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Logo className="h-6 w-6 text-primary" />
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            &copy; {new Date().getFullYear()} Aurumfolio. All rights reserved.
+            &copy; <CurrentYear /> Aurumfolio. All rights reserved.
           </p>
         </div>
         <div className="flex items-center gap-2">
